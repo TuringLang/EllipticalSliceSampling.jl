043564f94674ff00b19bca34a4d728744b1d7071
David Widmann <dev+git@devmotion.de>
2020-03-04 16:02:54 +0100

    Disable progress logging in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,4 @@
 using SafeTestsets
-using TerminalLoggers: TerminalLogger
-
-using Logging: global_logger
-
-global_logger(TerminalLogger())
 
 @safetestset "Simple tests" begin include("simple.jl") end
 @safetestset "GP regression tests" begin include("regression.jl") end
