efc3cd8622cd4abb6ad1bb0e7c32a4beb70d8f48
David Widmann <devmotion@users.noreply.github.com>
2021-10-19 00:55:17 +0200

    Set up documentation (#23)
    * Set up documentation
    
    * Update docs/make.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ docs/make.jl
@@ -0,0 +1,17 @@
+using Documenter
+using EllipticalSliceSampling
+
+makedocs(;
+    sitename="EllipticalSliceSampling",
+    format=Documenter.HTML(),
+    modules=[EllipticalSliceSampling],
+    pages=["Home" => "index.md"],
+    strict=true,
+    checkdocs=:exports,
+)
+
+deploydocs(;
+    repo="github.com/TuringLang/EllipticalSliceSampling.jl.git",
+    devbranch="main",
+    push_preview=true,
+)
